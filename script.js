27bbb6892913dc2ae883b6b1f499f38f8a97a7fd
NITDELHII <221210063@nitdelhi.ac.in>
2024-12-25 18:13:26 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('lgin').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
-    
+
     // Get the values from the input fields
     const username = document.getElementById('Rol').value;
     const password = document.getElementById('pass').value;
@@ -9,6 +9,9 @@ document.getElementById('lgin').addEventListener('submit', function(event) {
     if (username === "221310017" && password === "deepak17") {
         // Redirect to another page if credentials are correct
         window.location.href = "result.html"; // Change this to your desired page
+    } else if (username === "221320012" && password === "nikhil@#123") {
+        // Redirect to another page if credentials are correct
+        window.location.href = "resultnikhil.html"; // Change this to your desired page
     } else {
         // Show error message
         alert('Wrong Password');
